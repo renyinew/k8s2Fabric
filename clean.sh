3402e2d714194388507de7581ef06b83de388ea4
npcxu <375725304@qq.com>
2019-05-13 11:24:13 +0800

    update

---
+++ clean.sh
@@ -0,0 +1,45 @@
+
+KUBECONFIG_FOLDER=${PWD}/configFiles
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/blockchain-namespace.yaml
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/join_channel.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/create_channel.yaml
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/zookeeper-statefulset.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/kafka0.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/kafka1.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/kafka2.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/kafka3.yaml
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/orderer.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/peerOrg1.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/peerOrg2.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/peerOrg3.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/peerOrg4.yaml
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml
+kubectl delete -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
+
+kubectl delete -f ${KUBECONFIG_FOLDER}/createNfsVolume.yaml
+
+sleep 15
+
+echo -e "\npv:"
+kubectl get pv
+echo -e "\npvc:"
+kubectl get pvc
+echo -e "\njobs:"
+kubectl get jobs
+echo -e "\ndeployments:"
+kubectl get deployments
+echo -e "\nservices:"
+kubectl get services
+echo -e "\npods:"
+kubectl get pods
+
+echo -e "\nNetwork Cleaned!!\n"
