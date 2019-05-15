3a00c823b38b906797a92ce466a59d2d09cf8e32
npcxu <375725304@qq.com>
2019-05-15 15:51:06 +0800

    message

--- deployNetwork.sh
+++ deployNetwork.sh
@@ -39,93 +39,24 @@ else
     echo "The Persistant Volume exists, not creating again"
 fi
 
-# # Copy the required files(configtx.yaml, cruypto-config.yaml, sample chaincode etc.) into volume
-# echo -e "\nCreating Copy artifacts job."
-# echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml"
-# kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
-#
-# pod=$(kubectl get pods -A--selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
-#
-# podSTATUS=$(kubectl get pods -A--selector=job-name=copyartifacts --output=jsonpath={.items..phase} -A)
-#
-# while [ "${podSTATUS}" != "Running" ]; do
-#     echo "Wating for container of copy artifact pod to run. Current status of ${pod} is ${podSTATUS}"
-#     sleep 5;
-#     if [ "${podSTATUS}" == "Error" ]; then
-#         echo "There is an error in copyartifacts job. Please check logs."
-#         exit 1
-#     fi
-#     podSTATUS=$(kubectl get pods -A--selector=job-name=copyartifacts --output=jsonpath={.items..phase} -A)
-# done
-#
-# echo -e "${pod} is now ${podSTATUS}"
-# echo -e "\nStarting to copy artifacts in persistent volume."
-#
-# #fix for this script to work on icp and ICS
-# kubectl cp ./artifacts $pod:/shared/
-#
-# echo "Waiting for 10 more seconds for copying artifacts to avoid any network delay"
-# sleep 10
-# JOBSTATUS=$(kubectl get jobs -A -A|grep "copyartifacts" |awk '{print $2}')
-# while [ "${JOBSTATUS}" != "1/1" ]; do
-#     echo "Waiting for copyartifacts job to complete"
-#     sleep 1;
-#     PODSTATUS=$(kubectl get pods -A-A| grep "copyartifacts" | awk '{print $3}')
-#         if [ "${PODSTATUS}" == "Error" ]; then
-#             echo "There is an error in copyartifacts job. Please check logs."
-#             exit 1
-#         fi
-#     JOBSTATUS=$(kubectl get jobs -A -A|grep "copyartifacts" |awk '{print $2}')
-# done
-# echo "Copy artifacts job completed"
-
-# clean up the directory of crypto-config
-# echo -e "\nCreating artifacts job."
-# echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml"
-# kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
-#
-# pod=$(kubectl get pods -A--selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
-#
-# podSTATUS=$(kubectl get pods -A--selector=job-name=copyartifacts --output=jsonpath={.items..phase} -A)
-#
-# while [ "${podSTATUS}" != "Running" ]; do
-#     echo "Wating for container of copy artifact pod to run. Current status of ${pod} is ${podSTATUS}"
-#     sleep 5;
-#     if [ "${podSTATUS}" == "Error" ]; then
-#         echo "There is an error in copyartifacts job. Please check logs."
-#         exit 1
-#     fi
-#     podSTATUS=$(kubectl get pods -A--selector=job-name=copyartifacts --output=jsonpath={.items..phase} -A)
-# done
-
 # Generate Network artifacts using configtx.yaml and crypto-config.yaml
 echo -e "\nGenerating the required artifacts for Blockchain network"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/generateArtifactsJob.yaml
 
-JOBSTATUS=$(kubectl get jobs -A |grep utils|awk '{print $2}')
+JOBSTATUS=$(kubectl get jobs -A|grep "utils" |awk '{print $3}')
+job=$(kubectl get pods --selector=job-name=utils --output=jsonpath={.items..metadata.name} -A)
+podSTATUS=$(kubectl get pods --selector=job-name=utils --output=jsonpath={.items..phase} -A)
+
 while [ "${JOBSTATUS}" != "1/1" ]; do
-    echo "Waiting for generateArtifacts job to complete"
-    sleep 1;
-    # UTILSLEFT=$(kubectl get pods -A| grep utils | awk '{print $2}')
-    UTILSSTATUS=$(kubectl get pods -A| grep "utils" | awk '{print $3}')
-    if [ "${UTILSSTATUS}" == "Error" ]; then
-            echo "There is an error in utils job. Please check logs."
-            exit 1
+    echo "Wating for container of utils pod to run. Current status of ${job} is ${JOBSTATUS}"
+    sleep 5;
+    if [ "${podSTATUS}" == "Error" ]; then
+        echo "There is an error in utils job. Please check logs."
+        exit 1
     fi
-    # UTILSLEFT=$(kubectl get pods -A| grep utils | awk '{print $2}')
-    JOBSTATUS=$(kubectl get jobs -A |grep utils|awk '{print $2}')
+    JOBSTATUS=$(kubectl get jobs -A|grep "utils" |awk '{print $3}')
 done
-
-# Create namespace for all peers, ca, orderer, kafka, zookeeper
-echo -e "\nCreating Services for blockchain namespace"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/blockchain-namespace.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/blockchain-namespace.yaml
-
-# Create services for all peers, ca, orderer, kafka, zookeeper
-echo -e "\nCreating Services for blockchain network"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/blockchain-services.yaml
 
 # Create zookeeper using Kubernetes Deployments
 echo -e "\nCreating new Deployment to create three zookeepers and four kafkas in network"
